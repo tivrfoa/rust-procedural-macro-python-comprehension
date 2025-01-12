677ba7a4e38e3505abff41f1e1af5f518fa0ad86
tivrfoa <lescoutinhovr@gmail.com>
2025-01-12 09:21:32 -0300

    and && works fine! =)
    cargo expand
    
    fn test_and() {
        let a: Vec<i32> = core::iter::IntoIterator::into_iter(
                <[_]>::into_vec(::alloc::boxed::box_new([1, 2, 3])),
            )
            .filter_map(move |x| { (true && (x > 1 && x == 2)).then(|| x) })
            .collect();
        match a {
            tmp => {
                {
                    ::std::io::_eprint(
                        format_args!(
                            "[{0}:{1}:{2}] {3} = {4:#?}\n",
                            "src/main.rs",
                            36u32,
                            5u32,
                            "a",
                            &tmp,
                        ),
                    );
                };
                tmp
            }
        };
    }

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use comp::comp;
 fn main() {
     test_echo_no_if();
-    // test_echo_reference_no_if();
+    test_echo_reference_no_if();
 
     // test_single_if();
 
     // test_two_ifs();
 
-    // test_and();
+    test_and();
 }
 
 fn test_echo_no_if() {
@@ -28,5 +28,10 @@ fn test_echo_reference_no_if() {
 
 fn test_single_if() {
     let a: Vec<i32> = comp!(x for x in vec![1, 2, 3] if x > 1).collect();
+    dbg!(a);
+}
+
+fn test_and() {
+    let a: Vec<i32> = comp!(x for x in vec![1, 2, 3] if x > 1 && x == 2).collect();
     dbg!(a);
 }
